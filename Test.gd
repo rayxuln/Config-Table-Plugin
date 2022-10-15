9d4c8eeb8f1813309060116aa17794bae84fce7d
Raiix <imbosslx@sina.com>
2022-10-15 13:40:33 +0800

    Update config table tool.

--- Test.gd
+++ Test.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 func _ready() -> void:
-	var data = ConfigHelper.people.by_age(15)
-	print(data.name)
-	print(data.address)
-	print(ConfigHelper.people.by_name("张三").phone)
+    var data = ConfigHelper.people.by_age(15)
+    print(data.name)
+    print(data.address)
+    print(ConfigHelper.people.by_name("张三").phone)
